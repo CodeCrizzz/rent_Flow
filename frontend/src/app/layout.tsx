cbbbb46b0c0253c660ec5abd7051870b02ce0e7a
CodeCrizzz <natividadchristian670@gmail.com>
2026-04-30 21:36:20 +0800

    changing delay time of text color when toggling dark/light theme

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
       suppressHydrationWarning
     >
       <body className="min-h-full flex flex-col">
-        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
           {children}
         </ThemeProvider>
       </body>
